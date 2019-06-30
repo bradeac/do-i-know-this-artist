5cb658e704cb169ff778dea054555427bd6f8711
bradeac <bradeacamil@gmail.com>
2019-06-30 16:46:33 +0300

    Destructure variables. Add link to playlists

--- src/components/SearchResult/SearchResult.js
+++ src/components/SearchResult/SearchResult.js
@@ -3,19 +3,24 @@ import axios from 'axios'
 
 import { apiKey, getTracksDataUrl } from '../../config/environment'
 
+import './SearchResult.css'
+
 const TrackCard = React.lazy(() => import('../TrackCard/TrackCard'))
 
 const SearchResult = ({ playlist, query }) => {
     const [tracks, setTracks] = useState([])
 
+    const { playlistId } = playlist.id
+    const { title } = playlist.snippet
+
     useEffect(() => {
         async function findTracks() {
-            let url = `${getTracksDataUrl}${playlist.id.playlistId}${apiKey}`
+            let url = `${getTracksDataUrl}${playlistId}${apiKey}`
             const response = await axios(url)
             let tracks = [ ...response.data.items ]
 
             while (response.nextPagetoken) {
-                url = `${getTracksDataUrl}${playlist.id.playlistId}&pageToken=${response.nextPagetoken}&${apiKey}`
+                url = `${getTracksDataUrl}${playlistId}&pageToken=${response.nextPagetoken}&${apiKey}`
 
                 const nextResponse = await axios(url)
 
@@ -32,24 +37,33 @@ const SearchResult = ({ playlist, query }) => {
         }
 
         findTracks()
-    }, [playlist, query])
+    }, [playlist, playlistId, query])
 
     if (tracks.length > 0) {
         return (
             <>
-                <p>{playlist.snippet.title}</p>
+                <p>
+                    <a 
+                        className="playlist-title" 
+                        href={`https://www.youtube.com/playlist?list=${playlistId}`}
+                    >
+                        {title}
+                    </a>
+                </p>
                 
                 <Suspense
                     fallback={
                         <p>Fetching data ...</p>
                     }
                 >
-                    {tracks.map(track => (
-                        <TrackCard
-                            key={track.id}
-                            data={track}
-                        />
-                    ))}
+                    <div className="tracks">
+                        {tracks.map(track => (
+                            <TrackCard
+                                key={track.id}
+                                data={track}
+                            />
+                        ))}
+                    </div>
                 </Suspense>
             </>
         )
