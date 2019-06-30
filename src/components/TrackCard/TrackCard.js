49b0517ff1d02f316cb637a680353df4fee0e7f3
bradeac <bradeacamil@gmail.com>
2019-06-30 16:47:16 +0300

    Develop TrackCard component

--- src/components/TrackCard/TrackCard.js
+++ src/components/TrackCard/TrackCard.js
@@ -1,11 +1,37 @@
-import React from 'react'
+import * as React from 'react'
+
+import './TrackCard.css'
 
 const TrackCard = ({ data }) => {
-    const { title } = data.snippet
+    const { 
+        playlistId,
+        position, 
+        resourceId: { videoId }, 
+        thumbnails: { medium: { url } }, 
+        title, 
+    } = data.snippet
+
+    const trackUrl = `https://youtube.com/watch?v=${videoId}&list=${playlistId}&index=${position + 2}`
 
     return (
-        <div>
-            {title}
+        <div className="card-container">
+            <a 
+                href={trackUrl} 
+                className="card"
+            >
+                <div className="info">
+                    <img 
+                        alt="Track thumbnail"
+                        className="thumb-wrapper" 
+                        src={url}
+                    />
+                    <div className="meta">
+                        <div className="track-info">
+                            <div className="title">{title}</div>
+                        </div>
+                    </div>
+                </div>
+            </a>
         </div>
     )
 }
